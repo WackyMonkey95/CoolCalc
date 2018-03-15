4d8b4f12cb739f187f2563b921964278e8fefdec
DESKTOP-2N3KM0P\WackyMonkey <maikel-emmanuel@hotmail.nl>
2018-03-15 11:42:14 +0100

    init

---
+++ app/src/main/java/com/example/maike/coolcalc/CalcActivtie.java
@@ -0,0 +1,13 @@
+package com.example.maike.coolcalc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalcActivtie extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc_activtie);
+    }
+}
